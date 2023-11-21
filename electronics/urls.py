ae12f202df9f5b686b42c49532e58b4525d659c2
Andrés Pino <pinoandres85@gmail.com>
2023-11-20 22:49:37 -0500

    fix(errors): fix more shit here

--- electronics/urls.py
+++ electronics/urls.py
@@ -4,6 +4,6 @@ from .views import register_additional_client_info, home, product_page
 urlpatterns = [
     path('', home.home, name='base'),
     path('home/', home.home, name='go-home'),
-    path('registerAdditionalInfo/', register_additional_client_info.register_additional_information, name='register-additional-info'),
+    path('registerAdditionalInfo/', register_additional_client_info.register_additional_information, name='registerAdditionalInfo'),
     path('productPage/', product_page.product_page, name='product-page'),
 ]
